0290eb788b244393ff594faedfbbac15d4068e7d
Mrunali Deore <107094589+MrunaliDeore@users.noreply.github.com>
2022-08-24 16:38:19 +0530

    Refactor : Use GET Request Method and pass name as path variable

---
+++ HelloRestController.java
@@ -0,0 +1,24 @@
+package com.example.firstspringprogram.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloRestController {
+   //localhost:8080/hello
+    @RequestMapping("/hello")
+    public String getHome() {
+       return "Hello From Mrunali";
+    }
+
+    //localhost:8080/query?name=Mrunali
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String getByName(@RequestParam(value = "name") String name){
+       return "Hello "+ name;
+    }
+
+    //localhost:8080/param/Mrunali
+    @GetMapping("/param/{name}")
+    public String getByNameParam(@PathVariable String name){
+        return "Hello " + name;
+    }
+}
